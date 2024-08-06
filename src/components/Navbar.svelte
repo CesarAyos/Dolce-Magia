bc9cd6c20356e3a5169e1ba4401ad7600ec22ae6
CesarAyos <ayos007@gmail.com>
2024-08-06 13:39:40 -0400

    modify

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -34,7 +34,7 @@
           </div>
           <div class="col d-flex justify-content-center">
             <a class="navbar-brand" href="/">
-              <img src="static\c.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
+              <img src="/c.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
             </a>
           </div>
           <div class="col">
