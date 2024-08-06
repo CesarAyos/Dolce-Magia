48cb5638cef688f6ca6d647831f6d37a68f1be27
CesarAyos <ayos007@gmail.com>
2024-08-06 13:30:22 -0400

    modify

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,6 +2,7 @@
 
     import { onMount } from 'svelte';
     import { nombre } from './storage.js';
+    
   
     onMount(() => {
     if (!localStorage.getItem('nombre')) {
@@ -33,7 +34,7 @@
           </div>
           <div class="col d-flex justify-content-center">
             <a class="navbar-brand" href="/">
-              <img src="src\public\img\c.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
+              <img src="src/public/img/c.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
             </a>
           </div>
           <div class="col">
